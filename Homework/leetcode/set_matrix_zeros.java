59b6cef809c52443cc191a1ca78eca9992f49e70
Dharaheshh V <99342421+Dharaheshh@users.noreply.github.com>
2026-08-11 22:09:13 +0530

    leetcode homework-2 11-08-2026

---
+++ Homework/leetcode/set_matrix_zeros.java
@@ -0,0 +1,26 @@
+package leetcode;
+class set_matrix_zeros {
+    public void setZeroes(int[][] mat) {
+        int m = mat.length,n=mat[0].length;
+        boolean fr = false;
+        boolean fc = false;
+        for(int i=0;i<m;i++) if(mat[i][0]==0) fc = true;
+        for(int j=0;j<n;j++) if(mat[0][j]==0) fr = true;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(mat[i][j]==0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(mat[i][0]==0 || mat[0][j]==0) mat[i][j] = 0;
+            }
+        }
+        if(fr) for(int j=0;j<n;j++) mat[0][j] = 0;
+        if(fc) for(int i=0;i<m;i++) mat[i][0] = 0;
+
+    }
+}
