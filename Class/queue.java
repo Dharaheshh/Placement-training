0d381bd009ea6f44265bff139c0cd4cf15b53256
Dharaheshh V <99342421+Dharaheshh@users.noreply.github.com>
2026-08-25 15:42:23 +0530

    queue  implementation

---
+++ Class/queue.java
@@ -0,0 +1,13 @@
+package Class;
+import java.util.*;
+public class queue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        System.out.println(queue.peek());
+        System.out.println("Queue: " + queue);
+    }
+}
