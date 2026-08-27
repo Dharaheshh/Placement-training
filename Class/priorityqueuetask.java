72d8fd60a895b0724f949057ba3d295de66e9559
Dharaheshh V <99342421+Dharaheshh@users.noreply.github.com>
2026-08-27 11:09:08 +0530

    pq task

---
+++ Class/priorityqueuetask.java
@@ -0,0 +1,25 @@
+package Class;
+import java.util.*;
+class panel{
+    int panel_no,number_of_candidates;
+    String panel_name;
+    panel(int panel_no, String panel_name, int number_of_candidates){
+        this.panel_no = panel_no;
+        this.panel_name = panel_name;
+        this.number_of_candidates = number_of_candidates;
+    }
+}
+public class priorityqueuetask {
+    public static void main(String[] args) {
+        PriorityQueue<panel> q = new PriorityQueue<>((a,b) -> b.panel_no - a.panel_no);
+        q.add(new panel(1, "Panel 1", 10));
+        q.add(new panel(2, "Panel 2", 5));
+        q.add(new panel(3, "Panel 3", 15));
+        //print the output of the priority queue
+        System.out.println("Priority Queue: ");
+        while(!q.isEmpty()){
+            panel p = q.poll();
+            System.out.println("Panel No: " + p.panel_no + ", Panel Name: " + p.panel_name + ", Number of Candidates: " + p.number_of_candidates);
+        }
+    }
+}
